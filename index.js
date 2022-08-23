019b57b8b6bcdaa9b10c0c3230a2fe8635f2f863
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-08-23 23:37:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(toDo=`go to the office`) {
+    return `This Monday, I will ${toDo}.`;
+}
+mondayWork("work from home");
+
+function wrapAdjective(result = '*') {
+    return function(emphatic="special") {
+        return `You are ${result}${emphatic}${result}!`;
+    }
+}
+wrapAdjective()();
